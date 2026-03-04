b6c2c1e72997d7993c1ccdf1858200dd4b870515
francisco <fco_braga@hotmail.com>
2026-03-03 22:30:36 -0300

    finalizando variaveis

--- tipos-variaveis/src/tiposVariaveis.java
+++ tipos-variaveis/src/tiposVariaveis.java
@@ -1,5 +1,14 @@
 public class tiposVariaveis{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 1620.00;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 5;
+        System.out.println(numero);
+        final double VALOR_DE_PI = 3.14;// "final" garante que o valor da variavel em caixa alta n seja alterada
+        
     }
 }
